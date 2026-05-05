35b1ea5c02a15410558e297bfa9daec327194abc
Baniireza <135756325+Baniireza@users.noreply.github.com>
2026-05-05 17:38:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-bot = telebot.TeleBot('8686376012:AAFAx_gjj53cuLDRkr3LJsxclca6yhdMeC8')
+bot = telebot.TeleBot('8686376012:AAEj4x4GDEs-_4E9jbH5MbPrMYJwYEoWCco')
 @bot.message_handler(commands=['start'])
 def start(message): bot.reply_to(message, 'سلام! بوت کار کرد 🚀')
 bot.polling(none_stop=True)
