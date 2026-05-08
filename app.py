88e427e950f4d7eb358a55b211578f4e26ed2b25
Baniireza <135756325+Baniireza@users.noreply.github.com>
2026-05-08 14:22:09 +0330

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ client = InferenceClient(
     token=HF_TOKEN
 )
 
-MODEL_NAME = "meta-llama/Llama-3.1-8B-Instruct"
+MODEL_NAME = "meta-llama/Llama-3.1-8B-Instruct:novita"
 
 # ======================
 # SYSTEM PROMPT
